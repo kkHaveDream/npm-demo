cdd6015ee84ae508ccb075ff411508db359b03c0
xiaokk <824711747@qq.com>
2024-08-02 20:48:23 +0800

    新建npm包

---
+++ index.js
@@ -0,0 +1,29 @@
+// 节流函数
+function throttle(fn, delay) {
+  let timer = null;
+  return function (...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        timer = null;
+      }, delay);
+    }
+  };
+}
+
+// 防抖函数
+function debounce(fn, delay) {
+  let timer = null;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+// 导出函数
+module.exports = {
+  throttle,
+  debounce,
+};
